6fd49003889350c5f898dae66297feb370b2e6fc
liuqi <1781373476@qq.com>
2020-11-20 14:33:48 +0800

    第一次提交

---
+++ inde.py
@@ -0,0 +1,13 @@
+import os
+# type() yu isinstance()区别    type()不会认为子类是一种父类类型。 isinstance()会认为子类是一种父类类型。
+
+
+print  (__name__ )
+
+def  demohanshu():
+    print("测试函数成功")   
+
+
+
+if __name__ == "__main__":
+    demohanshu()
